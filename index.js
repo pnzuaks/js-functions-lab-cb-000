ea8c5a435a846b9a140f34ec1a8a3d30ed6428c9
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-14 22:25:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo() {
-
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
 }
 
 function happyHolidayTo() {
-  
+
 }
